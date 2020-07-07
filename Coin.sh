3337bfb7ff0666ebf7a507e3e4bdf9fd8f6810f0
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-07 12:22:13 +0530

    Added: fliping a coin muliple time through loop is added

--- Coin.sh
+++ Coin.sh
@@ -3,6 +3,7 @@ echo "This problem displays winning percentage of Head or Tail Combination in a 
 echo "As a Simulator start with Flipping a Coin to Display Heads or tails"
 heads=0
 tails=0
+
  no=$((RANDOM%2))
  if [ $no -eq 1 ]
   then
@@ -10,3 +11,16 @@ tails=0
  else
 	echo "tails win $no"
   fi
+
+#uc2
+echo " As a simulator, loop through Multiple times of flipping a coin and show number of times head and tail has won"
+for((i=1;i<10;i++))
+do
+ no=$((RANDOM%2))
+ if [ $no -eq 1 ]
+  then
+        echo "head win $no"
+ else
+        echo "tails win $no"
+  fi
+done
