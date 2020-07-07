45b88f687cf232afcffabb1923c73469777d99b4
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-07 12:12:24 +0530

    Added: fliping a coin is added

--- Coin.sh
+++ Coin.sh
@@ -1,1 +1,12 @@
 echo "This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
+#uc1
+echo "As a Simulator start with Flipping a Coin to Display Heads or tails"
+heads=0
+tails=0
+ no=$((RANDOM%2))
+ if [ $no -eq 1 ]
+  then
+	echo "head win $no"
+ else
+	echo "tails win $no"
+  fi
